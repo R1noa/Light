758949b3c819bc332fe37b8355618c3ecd8a2dac
R1noa <yuriy@milestep.io>
2017-02-01 12:40:30 +0200

    2 new tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 
 
 
-
 RSpec.describe Task, type: :model do
   before(:all) do
     @task = Task.new(title: "my new title")
   end
+
+ 
+ it "should do something" do
+		(1+1).should == 2
+	end
+
+  it "validates the name and makes sure it's not empty" do
+  	  task = Task.new(title:'')
+      task.valid?
+      task.errors[:title].should_not be_empty
+  end
+
   it 'we got what we need?' do
     expect(@task.title).to eq("my new title")
   end
