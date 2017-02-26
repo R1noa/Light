0879f6ed7af9559b544061b2735c82f4242c2e86
R1noa <yuriy@milestep.io>
2017-02-26 08:46:37 +0200

    Repair and Formatting

--- db/schema.rb
+++ db/schema.rb
@@ -37,5 +37,4 @@ ActiveRecord::Schema.define(version: 20170201154106) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
 end
